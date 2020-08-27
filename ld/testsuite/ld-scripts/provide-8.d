75e2ccc359e399a5155dc637e344a54c7e5d9fc6
Jeremy Drake <sourceware-bugzilla@jdrake.com>
2020-08-27 12:58:27 +0100

    Change the default characteristics of DLLs built by the linker to more secure settings.
    	PR 19011
    	* emultempl/pe.em (DEFAULT_DLL_CHARACTERISTICS): Define.
    	(pe_dll_characteristics): Initialise to DEFAULT_DLL_CHARACTERISTICS.
    	(add_options): Add options to disable DLL characteristics.
    	(list_options): List the new options.
    	(handle_options): Handle the new options.
    	* emultempl/pep.em: Similar changes to above.
    	(NT_EXE_IMAGE_BASE): Default to an address above 4G.
    	(NT_DLL_IMAGE_BASE, NT_DLL_AUTO_IMAGE_BASE,
    	(NT_DLL_AUTO_IMAGE_MASK): Likewise.
    	* ld.texi: Document the new options.
    	* pe-dll.c (pe_dll_enable_reloc_section): Change to default to
    	true.
    	(generate_reloc): Do nothing if there is no reloc section.
    	(pe_exe_fill_sections): Only assign the reloc section contents if
    	the section exists.
    	* testsuite/ld-pe/pe.exp: Add the --disable-reloc-section flag to
    	the .secrel32 tests.
    	* testsuite/ld-scripts/provide-8.d: Expect for fail on PE targets.
    	* NEWS: Mention the change in DLL generation.

--- ld/testsuite/ld-scripts/provide-8.d
+++ ld/testsuite/ld-scripts/provide-8.d
@@ -1,7 +1,7 @@
 #source: provide-5.s
 #ld: -T provide-8.t
 #nm: -B
-#xfail: x86_64-*-cygwin x86_64-*-msys mmix-*-* sh-*-pe spu-*-*
+#xfail: mmix-*-* *-*-pe spu-*-* *-*-mingw* *-*-cygwin *-*-msys
 
 #...
 0+4000 D __FOO
