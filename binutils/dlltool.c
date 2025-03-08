3e966bba35d9477afa432fca1d9e38367364bd45
Jeremy Drake <sourceware-bugzilla@jdrake.com>
2025-03-08 13:03:43 -0800

    move read-only delayimp data into .rdata
    This allows the delay IAT to be in its own section with nothing else, as
    required by IMAGE_GUARD_DELAYLOAD_IAT_IN_ITS_OWN_SECTION, documented at
    https://learn.microsoft.com/en-us/windows/win32/debug/pe-format#load-configuration-layout
    
    Signed-off-by: Jeremy Drake <sourceware-bugzilla@jdrake.com>

--- binutils/dlltool.c
+++ binutils/dlltool.c
@@ -2798,7 +2798,7 @@ make_delay_head (void)
 
   /* Output the delay import descriptor */
   fprintf (f, "\n%s DELAY_IMPORT_DESCRIPTOR\n", ASM_C);
-  fprintf (f, ".section\t.text$2\n");
+  fprintf (f, ".section\t.didat$2\n");
   fprintf (f, "%s __DELAY_IMPORT_DESCRIPTOR_%s\n", ASM_GLOBAL,imp_name_lab);
   fprintf (f, "__DELAY_IMPORT_DESCRIPTOR_%s:\n", imp_name_lab);
   fprintf (f, "\t%s 1\t%s grAttrs\n", ASM_LONG, ASM_C);
@@ -2844,8 +2844,6 @@ make_delay_head (void)
       fprintf (f, "\t.section\t.didat$4\n");
       fprintf (f, "__INT_%s:\n", imp_name_lab);
     }
-
-  fprintf (f, "\t.section\t.didat$2\n");
 
   fclose (f);
 
